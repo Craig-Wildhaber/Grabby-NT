401c33cd87ecb8d1ee53f3b9e067179a063a464e
Craig Wildhaber <107523955+Craig-Wildhaber@users.noreply.github.com>
2022-10-23 21:39:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 from pytube import YouTube
 
 home = os.environ["HOME"]
-directory = f"{home}/Music"
+directory = f"{home}/Downloads"
 link = input("What is the video link?: ")
 yt = YouTube(link)
 print("Title: ", yt.title)
-print("Title: ", yt.views)
-print("The video will be placed in your videos folder.")
+print("Views: ", yt.views)
+print("The video will be placed in your downloads folder.")
 yd = yt.streams.get_highest_resolution()
 print("Downloading...")
 yd.download(directory)
-print("Download complete!\nHere is the link to my Github\nif you want to view more of my repos!\n"
+print("Download complete!\nHere is the link to my Github\nif you want to view more of my repositories!\n"
       "https://github.com/Craig-Wildhaber")
